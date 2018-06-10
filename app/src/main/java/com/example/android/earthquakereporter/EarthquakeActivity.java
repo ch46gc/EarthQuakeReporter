3d51fb2c685ec9564b826dfa2bb5a84d2991a110
ch46gc <chriseyhillgen@gmail.com>
2018-06-09 23:22:10 -0400

    add preference screen

--- app/src/main/java/com/example/android/earthquakereporter/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/earthquakereporter/EarthquakeActivity.java
@@ -10,6 +10,8 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -128,8 +130,25 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderCallb
         mAdapter.clear();
 
     }
+   @Override
+    // This method initialize the contents of the Activity's options menu.
+    public boolean onCreateOptionsMenu(Menu menu){
+    // Inflate the Options Menu we specified in XML
+    getMenuInflater().inflate(R.menu.main,menu);
+    return true;
 }
+@Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        if(id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(this, SettingActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
 
+        }
+}
 
 
 
